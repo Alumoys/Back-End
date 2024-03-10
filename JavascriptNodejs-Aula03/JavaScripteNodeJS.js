d6da083275d0e0d932194985a58693437415b26d
Alumoys <128159774+Alumoys@users.noreply.github.com>
2024-03-10 14:24:50 -0300

    Add files via upload

---
+++ JavascriptNodejs-Aula03/JavaScripteNodeJS.js
@@ -0,0 +1,14 @@
+//tipagem dinâmica
+//untepyd
+
+let minhavar = 475;
+minhavarvar = "texto";
+minhaVar = true;
+
+// O que é o JavaScript?//
+/*
+.Tipagem dinâmica;
+.Por que se chama JS;;
+.O que é ES6/ ES2015;
+.Linguagem interpretada;
+*/
---
+++ JavascriptNodejs-Aula03/errosestacktrace.js
@@ -0,0 +1,21 @@
+/*
+Erro-1 ===================
+
+" const numero; " - SyntaxError: Missing initializer in const declaration. 
+Não foi definido um 'valor inicial'.
+A variavel do tipo const quando criada devem ser criadas com 'valor', não podendo ser vazio.
+corrçõa "const numero = 'algum valor';"
+
+Erro-2 ===================
+
+" console.log(numero " - SyntaxError: Missing ) after argument list.
+Não fechamento de uma lista de argumentos com ")".
+correção " console.log(numero) "
+
+Erro-3 ==================
+"const numero = 1; - ReferenceError : minharVar is not defind. 
+console.log(minhaVar);" A variavel minhaVar foi definida/cirada 
+correção "let minhaVar;
+console.log(minhaVar);"
+*/
+
