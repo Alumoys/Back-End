8c005f5610bd86ed17613b9f217cd82db823941d
Alumoys <128159774+Alumoys@users.noreply.github.com>
2024-04-19 09:53:01 -0300

    Add files via upload

---
+++ Exercícios-Aula03/exe05.js
@@ -0,0 +1,4 @@
+var minhasMusicas = ["Rock", "Samba", "Punk", "Funk", "Jazz"]
+for(var i = 0; i < minhasMusicas.length; i++){
+console.log(minhasMusicas[i]);
+};
---
+++ Exercícios-Aula03/exe06.js
@@ -0,0 +1,12 @@
+var livro = { 
+    titulo:1984,
+    Autor: "John Orwell",
+    N_paginas: 308,
+    Editora: "Ulisseia",
+    ISBN: 9780198185215,
+}
+console.log(`Livro ${livro.titulo}, Autor ${livro.Autor}, Nº paginas ${livro.N_paginas}, Editora ${livro.Editora}, Codigo da versão ${livro.ISBN}`)
+/*for(var i = 0; i < livro.length; i++){
+console.log(livro[i]);
+};
+*/
