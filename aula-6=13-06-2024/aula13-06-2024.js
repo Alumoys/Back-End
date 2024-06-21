506ad6843bb5c94a7630e7dfc9d88069ed215ab8
Alumoys <128159774+Alumoys@users.noreply.github.com>
2024-06-21 09:56:45 -0300

    Add files via upload

--- aula-6=13-06-2024/aula13-06-2024.js
+++ aula-6=13-06-2024/aula13-06-2024.js
@@ -1,3 +1,27 @@
+// function quickSort(arr){
+//     if(arr.length <= 1){
+//         return arr;
+//     }
+//     let pivot = arr[Math.floor(arr.length/2)];
+//     let left = [];
+//     let right = [];
+//     let middle = [];
+//     for(let element of arr){
+//         if( element < pivot){
+//             left.push(element);
+//         } else if(element === pivot){
+//             middle.push(element);
+//         } else{
+//             right.push(element);
+//         }
+//     }
+//     return quickSort(left).concat(middle).concat(quickSort(right));
+// }
+// var arr = [3,0,1,4,2,8,6,7,8];
+
+// console.log(quickSort(arr));
+
+
 function quickSort(arr){
     if(arr.length <= 1){
         return arr;
@@ -9,14 +33,19 @@ function quickSort(arr){
     for(let element of arr){
         if( element < pivot){
             left.push(element);
-        } else if(element === pivot){
+        }else if(element == pivot){
             middle.push(element);
-        } else{
+        }else{
             right.push(element);
         }
     }
     return quickSort(left).concat(middle).concat(quickSort(right));
 }
-var arr = [3,0,1,4,2,8,6,7,8];
 
-console.log(quickSort(arr));
+function main(){
+    let arr = [3,0,1,4,2,8,6,7]
+    console.log(quickSort(arr));
+    let sortedArr = quickSort(arr);
+    console.log("array ordenao" + sortedArr);
+}
+main();  // chamada da função main() para executar o programa.  // chamada
