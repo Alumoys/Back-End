6ab7bd0c8c1e2787fa19874329f21256cb689e7d
Alumoys <128159774+Alumoys@users.noreply.github.com>
2024-06-21 09:38:46 -0300

    Add files via upload

---
+++ aula-6=13-06-2024/aula13-06-2024.js
@@ -0,0 +1,22 @@
+function quickSort(arr){
+    if(arr.length <= 1){
+        return arr;
+    }
+    let pivot = arr[Math.floor(arr.length/2)];
+    let left = [];
+    let right = [];
+    let middle = [];
+    for(let element of arr){
+        if( element < pivot){
+            left.push(element);
+        } else if(element === pivot){
+            middle.push(element);
+        } else{
+            right.push(element);
+        }
+    }
+    return quickSort(left).concat(middle).concat(quickSort(right));
+}
+var arr = [3,0,1,4,2,8,6,7,8];
+
+console.log(quickSort(arr));
